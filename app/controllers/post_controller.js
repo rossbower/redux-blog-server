3a1777fc1ae9f7f6e9ba3784612c34ae7645c99c
Ross Bower <ross@ross-mbp.wireless.dartmouth.edu>
2016-08-08 13:23:04 -0400

    all functions working locally

---
+++ app/controllers/post_controller.js
@@ -0,0 +1,63 @@
+import Post from '../models/post_model';
+
+export const createPost = (req, res) => {
+  const post = new Post();
+  post.title = req.body.title;
+  post.tags = req.body.tags;
+  post.content = req.body.content;
+
+  post.save()
+  .then(result => {
+    res.json({ message: 'Post created!' });
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const getPosts = (req, res) => {
+  Post.find()
+  .then(result => {
+    const cleanPosts = (posts) => {
+      return posts.map(post => {
+        return { id: post._id, title: post.title, tags: post.tags };
+      });
+    };
+    res.json(cleanPosts(result));
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const getPost = (req, res) => {
+  Post.findById(req.params.id)
+  .then(result => {
+    res.json(result);
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const deletePost = (req, res) => {
+  Post.findById(req.params.id)
+  .then(result => {
+    result.remove();
+    res.json({ message: 'Post deleted' });
+  })
+  .catch(error => {
+    res.json({ error });
+  });
+};
+
+export const updatePost = (req, res) => {
+  Post.findById(req.params.id)
+  .then(result => {
+    if (req.body.title) result.title = req.body.title;
+    if (req.body.tags) result.tags = req.body.tags;
+    if (req.body.content) result.content = req.body.content;
+    result.save();
+  });
+  res.json({ message: 'Post updated' });
+};
--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import apiRouter from './router';
 
 // initialize
 const app = express();
@@ -24,3 +26,13 @@ const port = process.env.PORT || 9090;
 app.listen(port);
 
 console.log(`listening on: ${port}`);
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', apiRouter);
